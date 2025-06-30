e7d814d454bfae8062a52c5d0a3ad3ca5d408051
Gowtham kishore <sinki2029@gmail.com>
2025-06-30 15:32:20 +0530

    query params explored

--- src/app.js
+++ src/app.js
@@ -1,32 +1,17 @@
 const express = require("express");
 const app = express();
 
-
+// url: localhost:3000/user?firstName=Gowtham&lastName=kishore
 app.get("/user", (req, res) => {
+    console.log(req.query)   // Output: { firstName: 'Gowtham', lastName: 'kishore' }
     res.send({firstName:"Gowtham", lastName:"kishore"})
 })
 
-
-app.post("/user", (req, res) => {
-    res.send("Data successfully saved to Database!");
+// url: localhost:3000/user/12345
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params)  // Output: { userId: '12345' }
+    res.send({firstName:"Gowtham", lastName:"kishore"})
 })
-
-app.delete("/user", (req, res) => {
-    res.send("Deleted successfully");
-})
-
-
-app.use("/test", (req, res) => {
-    res.send("This is a test route!");
-})
-
-// app.use("/hello", (req, res) => {
-//     res.send("Hello, Everyone welcome...")
-// })
-
-// app.use("/", (req, res) => {
-//     res.send("Namaste Gowtham!");
-// })
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
