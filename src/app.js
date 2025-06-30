476df99cce06cb660831369a1b2a8162cc50319c
Gowtham kishore <sinki2029@gmail.com>
2025-06-30 15:10:37 +0530

    explored get, post, delete

--- src/app.js
+++ src/app.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("Hello, Everyone welcome...")
+
+app.get("/user", (req, res) => {
+    res.send({firstName:"Gowtham", lastName:"kishore"})
 })
+
+
+app.post("/user", (req, res) => {
+    res.send("Data successfully saved to Database!");
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully");
+})
+
 
 app.use("/test", (req, res) => {
     res.send("This is a test route!");
 })
 
-app.use("/", (req, res) => {
-    res.send("Namaste Gowtham!");
-})
+// app.use("/hello", (req, res) => {
+//     res.send("Hello, Everyone welcome...")
+// })
 
-
-
-
+// app.use("/", (req, res) => {
+//     res.send("Namaste Gowtham!");
+// })
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
