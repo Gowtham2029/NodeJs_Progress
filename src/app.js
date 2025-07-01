3ceaa13e0d027c80c4692a4dc8de1d8bf87d54dc
Gowtham kishore <sinki2029@gmail.com>
2025-07-01 11:28:53 +0530

    Database connection

--- src/app.js
+++ src/app.js
@@ -1,22 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/database");
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth")
 
-app.use("/admin", adminAuth)
-
-
-app.get("/user", userAuth, (req, res) => {
-    res.send("Welcome to the user page");
-})
-app.get("/admin/getAllDetails", (req, res) => {
-    res.send("Welcome to the user page");
-})
-
-app.get("/admin/DeleteUser", (req, res) => {
-    res.send("Deleted a user");
-})
-
-app.listen(3000, () => {
+connectDB().then(() => {
+    console.log("Database connected successfully");
+    app.listen(3000, () => {
     console.log("Server is running on port 3000");
+})
+}).catch((err) =>{
+    console.error("Database connection failed: ", err);
 })
