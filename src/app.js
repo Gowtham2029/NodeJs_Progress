914d0abf1c61d38e098e64a2d67d5b7308dc59c3
Gowtham kishore <sinki2029@gmail.com>
2025-07-01 23:13:49 +0530

    more about apis

--- src/app.js
+++ src/app.js
@@ -3,17 +3,19 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-app.post("/signup", async (req, res) =>{
-    const userObj = {
-        firstName: "Ram",
-        lastName:"Sita",
-        emailId: "SitaRam@gmail.com",
-        password: "password143",
-        age: 25,
-        gender: "Female",
-    }
+app.use(express.json());
 
-    const user = new User(userObj);
+app.post("/signup", async (req, res) =>{
+    // const userObj = {
+    //     firstName: "Ram",
+    //     lastName:"Sita",
+    //     emailId: "SitaRam@gmail.com",
+    //     password: "password143",
+    //     age: 25,
+    //     gender: "Female",
+    // }
+    // const userObj = req.body;
+    const user = new User(req.body);
 
     try{
        await user.save();
@@ -24,6 +26,36 @@ app.post("/signup", async (req, res) =>{
         res.status(500).send("Internal Server Error");
     }
    
+
+})
+
+
+// to get only one user by emailId
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const user = await User.find({emailId: userEmail})
+        res.send(user); 
+    }
+    catch(err){
+        console.error("Error fetching user: ", err);
+        res.status(400).send("Something went wrong");
+    }
+})
+
+// to get all users
+app.get("/feed", async (req, res) => {
+    
+
+    try{
+        const users = await User.find({})
+        res.send(users);
+    }
+    catch(err){
+        console.error("Error fetching users: ", err);
+        res.status(400).send("Something went wrong");
+    }
 
 })
 
